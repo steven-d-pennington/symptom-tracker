1d213ff268e75e480499b0718b5e5738b71f8bf6
Steven Pennington <stevepenn@hotmail.com>
2025-10-01 11:50:20 -0700

    Reset repository for fresh start - remove all existing code

--- app/(protected)/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function ProtectedIndexPage() {
-  redirect("/dashboard");
-}
--- middleware.ts
+++
@@ -1,5 +0,0 @@
-export { default } from "next-auth/middleware";
-
-export const config = {
-  matcher: ["/dashboard/:path*", "/log/:path*", "/medications/:path*", "/journal/:path*"],
-};
